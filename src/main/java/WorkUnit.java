1786e734a6deeaf545efb2ac4bbacb329407f1fb
Emil <KutEmil@gmail.com>
2022-06-23 11:52:58 +0300

    Small fixes

--- src/main/java/WorkUnit.java
+++ src/main/java/WorkUnit.java
@@ -1,13 +1,13 @@
 import ClassModels.Connection;
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
 
 import javax.swing.*;
-import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.util.List;
 import java.util.Map;
 
 public class WorkUnit {
@@ -17,12 +17,11 @@ public class WorkUnit {
         int ret = fileopen.showDialog(null, "Open file");
         if (ret == JFileChooser.APPROVE_OPTION)
         {
-            try
-            {
                 File file = fileopen.getSelectedFile();
                 FileInputStream fileInputStream = new FileInputStream(file);
                 Yaml yamlread = new Yaml();
                 Map<String, Object> data = yamlread.load(fileInputStream);
+
                 if (data == null)
                 {
                     JOptionPane.showMessageDialog(null, "Empty file selected");
@@ -30,17 +29,15 @@ public class WorkUnit {
                 {
                     for (Map.Entry<String, Object> s : data.entrySet())
                     {
-                        System.out.println(data);
+                        String key = s.getKey();
+                        System.out.println(key);
+                        List<Object> value = (List<Object>) s.getValue();
+                        for (Object o : value )
+                        {
+                            System.out.println(o);
+                        }
                     }
                 }
-            } catch (FileNotFoundException e)
-            {
-                throw new RuntimeException(e);
-            }
-            catch (HeadlessException e)
-            {
-                throw new RuntimeException(e);
-            }
         }
     }
 }
